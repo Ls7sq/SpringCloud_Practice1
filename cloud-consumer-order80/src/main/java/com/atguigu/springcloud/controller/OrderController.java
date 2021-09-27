a1d40e6be2b46cb2f111bfe7abc1637c26ef198a
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-09-27 13:02:21 +0800

    Ribbon LoadBalance + invoke RestTemplate
    getForEntity & getForObject
    postForEntity & postForObject

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +26,33 @@ public class OrderController {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
+    @GetMapping("/consumer/payment/createForEntity")
+    public CommonResult<Payment> create2(Payment payment){
+        ResponseEntity<CommonResult> entity
+                = restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()){
+            log.info(entity.getStatusCode()+"\t"+entity.getHeaders());
+            return entity.getBody();
+        }else{
+            return new CommonResult<>(444,"Failure operation!");
+        }
+    }
+
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()){
+            log.info(entity.getStatusCode()+"\t"+entity.getHeaders());
+            return entity.getBody();
+        }else{
+            return new CommonResult<>(444,"Failure operation!");
+        }
     }
 }
