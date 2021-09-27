bb652099161f7a7157f7ba7df13c86d241d1a81b
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-09-27 16:40:46 +0800

    Write the RoundRobinRule

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContentConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContentConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContentConfig {
 
     @Bean
-    @LoadBalanced //赋予RestTemplate负载均衡的能力
+//    @LoadBalanced //赋予RestTemplate负载均衡的能力
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/lb/MyLB.java
@@ -0,0 +1,33 @@
+package com.atguigu.springcloud.lb;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component//以至于可以在springcloud这个package下扫的到
+public class MyLB implements LoadBalancer{
+
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    public final int getAndIncrement(){
+        int current;
+        int next;
+
+        do {
+            current = this.atomicInteger.get();
+            next = current >= Integer.MAX_VALUE ? 0 : current + 1;
+        }while (!this.atomicInteger.compareAndSet(current,next));
+        System.out.println("********The time of access, next: " + next);
+        return next;
+    }
+
+    //rest接口当前请求的次数 % 服务器集群总数量 = 实际调用服务器的index，每次服务重启之后，rest接口计数从1开始
+    @Override
+    public ServiceInstance instances(List<ServiceInstance> serviceInstances) {
+
+        int index = getAndIncrement() % serviceInstances.size();
+        return serviceInstances.get(index);
+    }
+}
