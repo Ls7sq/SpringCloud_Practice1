3f1bc10939714455302aa316844f71700e46d0b4
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-09-28 13:45:02 +0800

    Hystrix provider service fallbackMethod

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -11,16 +13,26 @@ public class PaymentService {
                 + "\t" +" paymentInfo_OK, id: " + id + "\t";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler", commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String paymentInfo_Timeout(Integer id){
 
-        int timeNumber = 3;
-        try{
-            TimeUnit.SECONDS.sleep(timeNumber);
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        }
+        int age = 10/0;
+        int timeNumber = 5;
+//        try{
+//            TimeUnit.SECONDS.sleep(timeNumber);
+//        }catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
         return "Thread Pool: " + Thread.currentThread().getName()
                 + "\t" +" paymentInfo_Timeout, id: " + id + "\t"
                 + "time consuming: " + timeNumber;
+    }
+
+    public String paymentInfo_TimeoutHandler(Integer id){
+        return "Thread Pool: " + Thread.currentThread().getName()
+                + "\t" +" paymentInfo_TimeoutHandler, id: " + id + "\t"
+                + "Sorry Timeout or operation abnormal";
     }
 }
