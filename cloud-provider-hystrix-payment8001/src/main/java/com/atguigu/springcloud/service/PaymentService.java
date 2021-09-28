a77a5b9caabbf14d0e567cf4e244f5c8991c49ef
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-09-28 20:01:58 +0800

    Circuit breaker_1

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,9 +1,12 @@
 package com.atguigu.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -12,7 +15,7 @@ public class PaymentService {
         return "Thread Pool: " + Thread.currentThread().getName()
                 + "\t" +" paymentInfo_OK, id: " + id + "\t";
     }
-
+    //====fallback
     @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler", commandProperties = {
             @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "5000")
     })
@@ -34,5 +37,26 @@ public class PaymentService {
         return "Thread Pool: " + Thread.currentThread().getName()
                 + "\t" +" paymentInfo_TimeoutHandler, id: " + id + "\t"
                 + "Sorry 8001 Timeout or operation abnormal";
+    }
+
+    //====circuit breaker
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback", commandProperties = {
+            @HystrixProperty(name="circuitBreaker.enabled",value="true"),
+            @HystrixProperty(name="circuitBreaker.requestVolumeThreshold",value = "10"),
+            @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds",value = "10000"),
+            @HystrixProperty(name="circuitBreaker.errorThresholdPercentage",value = "60")
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        if (id < 0){
+            throw new RuntimeException("*********id can not load");
+        }
+        String serialNumber = IdUtil.simpleUUID();  //UUID.randomUUID().toString()
+
+        return Thread.currentThread().getName()
+                + "\t" + "successful, serial number: " + serialNumber;
+    }
+
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id){
+        return "id can not load, please try later. id: " + id;
     }
 }
