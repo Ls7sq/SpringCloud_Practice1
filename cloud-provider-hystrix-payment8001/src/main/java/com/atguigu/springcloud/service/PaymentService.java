bf1efcf9e06b274c62fd31b830800e7437422ee8
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-09-28 09:34:51 +0800

    Hystrix Provider service set up

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class PaymentService {
+    public String paymentInfo_OK(Integer id){
+        return "Thread Pool: " + Thread.currentThread().getName()+" paymentInfo_OK, id: " + id + "\t";
+    }
+
+    public String paymentInfo_Timeout(Integer id){
+
+        int timeNumber = 3;
+        try{
+            TimeUnit.SECONDS.sleep(timeNumber);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return "Thread Pool: " + Thread.currentThread().getName()+" paymentInfo_Timeout, id: " + id + "\t"
+                + "time consuming: " + timeNumber;
+    }
+}
