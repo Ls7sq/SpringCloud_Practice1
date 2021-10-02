ff8a3aab38a21094025d24b274fb755dffbbcfea
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-02 11:18:03 +0800

    Sentinel Thread

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -3,11 +3,19 @@ package com.atguigu.springcloud.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class FlowLimitController {
 
     @GetMapping("/testA")
     public String testA(){
+
+        try{
+            TimeUnit.MILLISECONDS.sleep(2000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         return "---------testA";
     }
 
