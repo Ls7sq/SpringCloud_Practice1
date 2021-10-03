fcd6fd9bf9f8ad5a65ecc32defb6b860bc46585d
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-03 21:05:11 +0800

    continue seata-order-service: Order Module

--- seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/domain/dao/OrderDao.java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.springcloud.alibaba.domain.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public class OrderDao {
-}
---
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/service/impl/OrderServiceImpl.java
@@ -0,0 +1,47 @@
+package com.atguigu.springcloud.alibaba.service.impl;
+
+import com.atguigu.springcloud.alibaba.dao.OrderDao;
+import com.atguigu.springcloud.alibaba.domain.Order;
+import com.atguigu.springcloud.alibaba.service.AccountService;
+import com.atguigu.springcloud.alibaba.service.OrderService;
+import com.atguigu.springcloud.alibaba.service.StorageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+
+@Service
+@Slf4j
+public class OrderServiceImpl implements OrderService {
+
+    @Resource
+    private OrderDao orderDao;
+
+    @Resource
+    private StorageService storageService;
+
+    @Resource
+    private AccountService accountService;
+
+    @Override
+    public void create(Order order) {
+        log.info("----->Initialize Order creating");
+        orderDao.create(order);
+
+        log.info("----->Order service invoke Storage service, Count refresh");
+        storageService.decrease(order.getProductId(),order.getCount());
+        log.info("----->Order service invoke Storage service, Count refresh successful");
+
+        log.info("----->Order service invoke Account service, Money decrease");
+        accountService.decrease(order.getUserId(), order.getMoney());
+        log.info("----->Order service invoke Account service, Money decrease successful");
+
+        //修改订单状态 0 -> 1
+        log.info("----->Update Order status");
+        orderDao.update(order.getUserId(),0);
+        log.info("----->Update Order status successful");
+
+        log.info("----->Order finish");
+    }
+}
