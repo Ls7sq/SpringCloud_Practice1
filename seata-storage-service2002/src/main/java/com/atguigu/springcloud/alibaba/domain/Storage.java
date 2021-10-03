8d1c273e12f41f350a24f6ec362a3840658e3893
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-03 21:51:09 +0800

    Seata Account Module

---
+++ seata-storage-service2002/src/main/java/com/atguigu/springcloud/alibaba/domain/Storage.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud.alibaba.domain;
+
+public class Storage {
+
+    private Long id;
+
+    private Long productId;
+
+    private Integer total;
+
+    private Integer used;
+
+    private Integer residue;
+}
