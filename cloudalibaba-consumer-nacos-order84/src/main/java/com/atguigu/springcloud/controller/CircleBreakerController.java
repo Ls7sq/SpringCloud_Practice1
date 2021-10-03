7ec68dc66a9f86d5c6d586e29aa6f849cb61caad
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-03 09:53:14 +0800

    Sentinel CircuitBreaker add fallback

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/controller/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/controller/CircleBreakerController.java
@@ -20,13 +20,13 @@ public class CircleBreakerController {
     private RestTemplate restTemplate;
 
     @RequestMapping("/consumer/fallback/{id}")
-    //@SentinelResource(value = "fallback") //没有配置
-    //@SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
+//    @SentinelResource(value = "fallback") //没有配置
+    @SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
     //@SentinelResource(value = "fallback", blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
-    @SentinelResource(value = "fallback",
-            fallback = "handlerFallback",
-            blockHandler = "blockHandler",
-            exceptionsToIgnore = {IllegalArgumentException.class})
+//    @SentinelResource(value = "fallback",
+//            fallback = "handlerFallback",
+//            blockHandler = "blockHandler",
+//            exceptionsToIgnore = {IllegalArgumentException.class})
     public CommonResult<Payment> fallback(@PathVariable Long id) {
         log.info("id===" + id);
         CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/" + id, CommonResult.class, id);
@@ -40,4 +40,9 @@ public class CircleBreakerController {
         return result;
     }
 
+    //fallback
+    public CommonResult handlerFallback(@PathVariable Long id, Throwable e){
+        Payment payment = new Payment(id,null);
+        return new CommonResult(444,"兜底异常handler, exception内容： " + e.getMessage(), payment);
+    }
 }
